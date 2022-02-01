08c703a8c4356fb2797cb054c1a7e4e9f0ff2c77
Robby1567 <robby1567@gmail.com>
2022-01-31 18:38:15 -0600

    fixed users router

--- api/routes/users.js
+++ api/routes/users.js
@@ -16,10 +16,11 @@ router.get('/list', function(req, res) {
 
 // create new user
 router.post('/new', function(req, res) {
-  let sql = `INSERT INTO users(name, gender) VALUES (?)`;
+  let sql = `INSERT INTO users(name, username, password) VALUES (?)`;
   let values = [
     req.body.name,
-    req.body.gender
+    req.body.username,
+    req.body.password
   ];
   db.query(sql, [values], function(err, data, fields) {
     if (err) throw err;
@@ -29,5 +30,18 @@ router.post('/new', function(req, res) {
     })
   })
 });
+
+// 1) Receive POST request containing the usernam and password from the login form
+
+// 2) Try to lookup Usernam in database
+//    - If there is no match, return an error -- HTTP 401: Also return error message on the response "Could not find matching username"
+//    - if there is a match....
+
+// 3) Loockup password for the corresponding username and compare to the password sent with the request
+//    - If the pw's don't match, return an error
+//    - If the pw's do match
+
+// 4) Send success 
+
 
 module.exports = router;
