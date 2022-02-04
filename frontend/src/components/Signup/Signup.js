4b00fc3faaa61999ad45e83e9f4aeb74f1a30f14
Myriam D <diabagatemyriam@gmail.com>
2022-02-03 16:40:23 -0800

    bug fixes in cart

--- frontend/src/components/Signup/Signup.js
+++ frontend/src/components/Signup/Signup.js
@@ -1,6 +1,11 @@
 import React, {useState} from 'react';
+import useStyles from './styles';
+import { Card, CardActions, CardHeader, CardContent } from '@material-ui/core';
 
 const Signup = () => {
+  const classes = useStyles()
+
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassWord] = useState("");
@@ -34,49 +39,53 @@ const Signup = () => {
   
   
   return (
+      
+    <form className={classes.container} noValidate autoComplete="off">
+      <Card className={classes.card}>
+        <CardHeader className={classes.header} title="Create an Account" />
+        <CardContent>
+          <div className={classes.field}>
+            <input
+              type="text"
+              value={name}
+              placeholder="Username"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
 
-    
-    <div className="text-center m-5-auto">
-  <h2>Join us</h2>
-  <h5>Create An Account</h5>
+          <div className={classes.field}>
+            <input
+              type="text"
+              value={email}
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className={classes.field}>
+            <input
+              type="text"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => setPassWord(e.target.value)}
+            />
+          </div>
+          <div>
+              <input type="checkbox" name="checkbox" id="checkbox" required /> <span>I agree to all the statements in <a href='https://en.wikipedia.org/wiki/Terms_of_service#:~:text=Terms%20of%20service%20(also%20known,to%20use%20the%20offered%20service' target="_blank" rel="noopener noreferrer">terms of service</a></span>.
+          </div>
+        
+
+        <CardActions>
+          <div>
+              <button className={classes.subBtn} type="submit">Register</button>
+          </div>
+          <div className="message">{message ? <p>{message}</p> : null}</div>
+          </CardActions>
+          </CardContent>
+      </Card>
+    </form>
   
-  <form onSubmit={handleSubmit} >
-      <div>
-        <input
-          type="text"
-          value={name}
-          placeholder="Username"
-          onChange={(e) => setName(e.target.value)}
-        />
-      </div>
 
-      <div>
-        <input
-          type="text"
-          value={email}
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <input
-          type="text"
-          value={password}
-          placeholder="Password"
-          onChange={(e) => setPassWord(e.target.value)}
-        />
-      </div>
-      <div>
-          <input type="checkbox" name="checkbox" id="checkbox" required /> <span>I agree all statements in <a href='https://en.wikipedia.org/wiki/Terms_of_service#:~:text=Terms%20of%20service%20(also%20known,to%20use%20the%20offered%20service' target="_blank" rel="noopener noreferrer">terms of service</a></span>.
-      </div>
-      <div>
-          <button  type="submit">Register</button>
-      </div>
-      <div className="message">{message ? <p>{message}</p> : null}</div>
-  </form>
-  
-</div>
   )};
 
 export default Signup;
